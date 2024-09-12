e9ff242adc35d97ce2e27f8108e194ffd49226ab
Inzamam Ahmad <enzee274@gmail.com>
2024-09-12 20:04:00 +0500

    12/09/2024

--- index.js
+++ index.js
@@ -3,3 +3,24 @@ const fs = require("fs");
 const data = fs.readFileSync("a.txt", "utf-8");
 
 console.log(data)
+
+
+function addition(a,b){
+    return a + b;
+}
+
+function multiplication (a,b){
+    return a*b;
+}
+
+function subtraction(a,b){
+    return a - b;
+}
+
+function operation(a,b,op){
+    return op(a,b)
+}
+
+var ans = operation(1,4,addition)
+
+console.log(ans)
---
+++ promise.js
@@ -0,0 +1,2 @@
+// in this code. I'll leaarn about promises. We're hoping to learn this early. Async, await
+
