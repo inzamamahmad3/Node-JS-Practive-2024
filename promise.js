8260968f65f6203510620948b39f4b6e16724421
Inzamam Ahmad <enzee274@gmail.com>
2024-09-17 00:58:29 +0500

    learnt

--- promise.js
+++ promise.js
@@ -1,2 +1,32 @@
 // in this code. I'll leaarn about promises. We're hoping to learn this early. Async, await
 
+// Classes in Javascript
+
+class Rectangle{
+    constructor(width, height, color) {
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+    
+    area() {
+        const area = this.width * this.height;
+        return area;
+    }
+    
+    paint() {
+        console.log(`Color is ${this.color}`);
+    }
+}                                                                                                                                    
+
+let rect = new Rectangle(2,3, "red");
+let area1 = rect.area();
+console.log(area1)
+
+// Map
+
+const map = new Map();
+map.set('name', 'alice');
+map.set ('age', 30);
+
+console.log(map.get('name'))
